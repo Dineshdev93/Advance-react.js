266a953ddecdfd68562189b8e10fcf963935e966
Dinesh Sharma <dineshsharmadev93@gmail.com>
2023-05-01 08:47:50 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import User from "./Components.js/User";
+import UserDetails from "./UserDetails";
+function App() {
+ 
+  return (
+    <div className="App">
+       <h1>hemant</h1>
+    </div>
+  );
+}
+
+export default App;
+
